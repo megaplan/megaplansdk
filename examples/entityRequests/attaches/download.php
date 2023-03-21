8e0e0c0cd0dadfac0d135704f5c7a942aebbba7d
Sergey Preobrazhensky <s.preobrazhensky@megaplan.ru>
2023-03-22 02:31:28 +0300

    Добавил пример скачивания вложения

---
+++ examples/entityRequests/attaches/download.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  Скачивание вложения
+ */
+
+use Megaplan\Megaplansdk\Logger\LoggerInterface;
+use Megaplan\Megaplansdk\MegaplanRequest;
+/**
+ *  @var MegaplanRequest $request
+ *  @var  LoggerInterface $logger
+ */
+
+// Получаем из поля типа Attache
+$fileId = 87;
+$fileName = 'test.ods';
+
+$result = $request->get('/api/v3/attache/shareUrl/'.$fileId);
+$data = $result->getData();
+$logger->info('link', $data->data->link);
+file_put_contents(
+    '/tmp/'.$fileName,
+    file_get_contents($data->data->link)
+);
